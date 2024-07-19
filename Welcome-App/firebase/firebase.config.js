8d7eb0ff7915a2cfc938d66dbaca5c72e7935150
FabrizioZeballos <zeballosfab@gmail.com>
2024-07-19 16:21:37 -0300

    Configure environment variables

--- Welcome-App/firebase/firebase.config.js
+++ Welcome-App/firebase/firebase.config.js
@@ -1,22 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAN2Rw8UinXLXHCh-hjrrHu1TYZLi-rFVA",
-  authDomain: "welcome-app-6a028.firebaseapp.com",
-  projectId: "welcome-app-6a028",
-  storageBucket: "welcome-app-6a028.appspot.com",
-  messagingSenderId: "1079820519621",
-  appId: "1:1079820519621:web:5687601c78237d7d4148ac",
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 const auth = getAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
